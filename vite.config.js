a4f406642fe15640824c09a070c40a38e5e818d4
henhal <henhalvor@gmail.com>
2023-10-08 17:31:57 +0200

    add vite config

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+export default {
+    // Basic options
+    base: '/', // Specify the base URL for your project (default is '/')
+  
+    // Development server options
+    server: {
+      port: 3000, // Port for the development server (default is 3000)
+    },
+  
+    // Build options
+    build: {
+      // Output directory for the production build (default is 'dist')
+      outDir: 'dist',
+    },
+  };
+  
